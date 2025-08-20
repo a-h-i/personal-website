0c849277eb36d08f5adcbb905ec96e5df81abb55
Ahmed Ismail <ahm3d.hisham@gmail.com>
2025-08-20 06:15:21 +0100

    pulumi amplify logging policy

--- deploy/index.ts
+++ deploy/index.ts
@@ -25,6 +25,7 @@ const serviceRoleTrust = pulumi.all([acct, region]).apply(([_, r]) =>
             `amplify.${r}.amazonaws.com`,
             'amplifybackend.amazonaws.com',
             'codebuild.amazonaws.com',
+            `codebuild.${r}.amazonaws.com`,
           ],
         },
         Action: 'sts:AssumeRole',
@@ -39,42 +40,100 @@ const amplifyRole = new aws.iam.Role('amplifyServiceRole', {
 });
 
 // Give Amplify the managed admin policy for hosting
-const amplifyAdmin = new aws.iam.RolePolicyAttachment('amplifyAdmin', {
-  role: amplifyRole.name,
-  policyArn: 'arn:aws:iam::aws:policy/AdministratorAccess-Amplify',
+const amplifyAdmin = new aws.iam.RolePolicyAttachment(
+  'amplifyAdmin',
+  {
+    role: amplifyRole.name,
+    policyArn: 'arn:aws:iam::aws:policy/AdministratorAccess-Amplify',
+  },
+  {
+    dependsOn: [amplifyRole],
+  },
+);
+const logsPolicyDoc = pulumi.all([acct, region]).apply(([a, r]) =>
+  JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Sid: 'WriteToAmplifyLogGroups',
+        Effect: 'Allow',
+        Action: [
+          'logs:CreateLogStream',
+          'logs:PutLogEvents',
+          'logs:DescribeLogStreams',
+        ],
+        Resource: `arn:aws:logs:${r}:${a}:log-group:/aws/amplify/*:log-stream:*`,
+      },
+      {
+        Sid: 'CreateAndDiscoverGroups',
+        Effect: 'Allow',
+        Action: ['logs:CreateLogGroup', 'logs:DescribeLogGroups'],
+        Resource: '*',
+      },
+      {
+        Sid: 'OptionalRetention',
+        Effect: 'Allow',
+        Action: 'logs:PutRetentionPolicy',
+        Resource: `arn:aws:logs:${r}:${a}:log-group:/aws/amplify/*`,
+      },
+    ],
+  }),
+);
+const amplifyServiceLogsPolicy = new aws.iam.Policy('amplifyLogs', {
+  description:
+    'Allow Amplify service role to create/write CloudWatch Logs under /aws/amplify/*',
+  policy: logsPolicyDoc,
 });
 
-const app = new aws.amplify.App('personal-website', {
-  name: 'personal-website',
-  repository: 'https://github.com/a-h-i/personal-website',
-  accessToken: githubToken,
-  platform: 'WEB_COMPUTE',
-  buildSpec: `version: 1
-    frontend:
-        phases:
-            preBuild:
-                commands:
-                - npm -g install pnpm
-                - pnpm install
-            build:
-                commands:
-                    - pnpm build
-        artifacts:
-            baseDirectory: .next
-            files:
-                - '**/*'
-        cache:
-            paths:
-                - node_modules/**/*
+const amplifyServiceLogsAttach = new aws.iam.RolePolicyAttachment(
+  'amplifyServiceLogsAttach',
+  {
+    role: amplifyRole.name,
+    policyArn: amplifyServiceLogsPolicy.arn,
+  },
+  {
+    dependsOn: [amplifyRole],
+  },
+);
+
+const app = new aws.amplify.App(
+  'personal-website',
+  {
+    name: 'personal-website',
+    repository: 'https://github.com/a-h-i/personal-website',
+    accessToken: githubToken,
+    platform: 'WEB_COMPUTE',
+    buildSpec: `
+version: 1
+frontend:
+    phases:
+        preBuild:
+            commands:
+            - npm -g install pnpm
+            - pnpm install
+        build:
+            commands:
+                - pnpm build
+    artifacts:
+        baseDirectory: .next
+        files:
+            - '**/*'
+    cache:
+        paths:
+            - node_modules/**/*
                 
     `,
-  environmentVariables: {
-    NEXT_PUBLIC_HOST: `https://${domain}`,
-    NEXT_PUBLIC_POSTHOG_KEY: posthogKey,
-    NEXT_PUBLIC_POSTHOG_HOST: posthogHost,
+    environmentVariables: {
+      NEXT_PUBLIC_HOST: `https://${domain}`,
+      NEXT_PUBLIC_POSTHOG_KEY: posthogKey,
+      NEXT_PUBLIC_POSTHOG_HOST: posthogHost,
+    },
+    iamServiceRoleArn: amplifyRole.arn,
   },
-  iamServiceRoleArn: amplifyRole.arn,
-});
+  {
+    dependsOn: [amplifyAdmin, amplifyServiceLogsAttach],
+  },
+);
 const main = new aws.amplify.Branch(
   'main',
   {
